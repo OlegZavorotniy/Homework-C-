37be76d745d4ae46722c7fa6a1b280ddf3581942
Oleg Zavorotniy <o.zavorotniy@hotmail.com>
2024-02-01 16:55:35 +0300

    Add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача №3: Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+void PrintElemetsArray(int index, int[] arr)
+{
+    if(index == arr.Length) return;
+    PrintElemetsArray(index+1, arr);
+    Console.Write($"{arr[index]} ");
+}
+
+
+int[] array = {1, 2, 5, 0, 10 ,34};
+PrintElemetsArray(0, array);
